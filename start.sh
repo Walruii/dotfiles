bc22db97daf4780cdeffd1221db66b19b0e96b6c
walruii <walruiix@gmail.com>
2024-06-07 20:15:28 +0530

    start sh push

--- start.sh
+++ start.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 # getting the os
-var=`cat /etc/os-release | grep ID_LIKE`
+var=$(grep ID_LIKE /etc/os-release)
 packages="git neovim zip unzip zoxide clangd gcc gh zsh lua5.4 man nodejs python3 tmux"
 
-if [ $var = 'ID_LIKE=debian'  ]; then
+if [[ "$var" == *"ID_LIKE=debian"* ]]; then
   # if debian based then update it
   sudo apt-get update
-  sudo apt-get install "$packages"
+  sudo apt-get install -y $packages
 else 
   # then it is arch so update that
-  sudo pacman -Syy
-  sudo pacman "-S $packages"
+  sudo pacman -Syu --noconfirm $packages
 fi
 
 # install nodejs / language support
-sudo npm install "-g" npm@latest
-npm i "-g vscode-langservers-extracted typescript typescript-language-server @tailwindcss/language-server"
+sudo npm install -g npm@latest
+sudo npm install -g vscode-langservers-extracted typescript typescript-language-server @tailwindcss/language-server
 
 # config files for nvim and zsh
 git clone https://github.com/walruii/nvim-arch.git ~/.config/nvim
@@ -24,7 +23,7 @@ git clone https://github.com/walruii/dotfiles.git ~/.config/dotfiles
 mv ~/.config/dotfiles/.zshrc ~/
 
 # installing oh my zsh and setting up config
-sh "-c" "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp ~/.config/dotfiles/daivasmara.zsh-theme ~/.oh-my-zsh/custom/themes/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
